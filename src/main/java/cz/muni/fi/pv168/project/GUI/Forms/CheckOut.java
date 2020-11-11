2dd7ba712616ea3c5e530d1c7f056096cbb662c9
xkostik <xkostik@fi.muni.cz>
2020-11-11 22:16:33 +0100

    Edited Check-out form gui, added formatted text and "close" button

--- src/main/java/cz/muni/fi/pv168/project/GUI/Forms/CheckOut.java
+++ src/main/java/cz/muni/fi/pv168/project/GUI/Forms/CheckOut.java
@@ -1,17 +1,43 @@
 package cz.muni.fi.pv168.project.GUI.Forms;
 
-import javax.swing.*;
+import cz.muni.fi.pv168.project.GUI.Button;
 
-public class CheckOut extends Form {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class CheckOut extends Form implements ActionListener {
+    Dimension dimension = new Dimension(250, 200);
+    JButton closeButton;
+    String receipt = "<html>Client: %s<br/><br/>" +
+            "Nights spent: %d<br/>" +
+            "Room cost per night: %d<br/>" +
+            "Local fees per person per night: %d<br/><br/>" +
+            "<u>Total: %d</u></html>";
+
     public CheckOut() {
         super("Check-out");
-        this.setSize(250, 200);
-        String receipt = "<html>Nights spent: %d<br/>" +
-                "Room cost per night: %d<br/>" +
-                "Local fees per person per night: %d<br/>" +
-                "<u>Total: %d</u></html>";
+        this.setSize(dimension);
+        this.setResizable(false);
+        this.setLayout(new BorderLayout(0, 0));
 
-        JLabel label = new JLabel(String.format(receipt, 3, 1200, 50, 5200), SwingConstants.CENTER);
-        this.add(label);
+        JLabel label = new JLabel(String.format(receipt, "Petr Novak", 3, 1200, 50, 5200), SwingConstants.CENTER);
+        this.add(label, BorderLayout.CENTER);
+
+        closeButton = new Button("Close");
+        closeButton.setMaximumSize(new Dimension(100, 30));
+        closeButton.addActionListener(this);
+        JPanel panel = new JPanel();
+        panel.add(closeButton);
+
+        this.add(panel, BorderLayout.SOUTH);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == closeButton) {
+            onClose();
+        }
     }
 }
