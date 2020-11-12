850893553a17dd41be90614be3cd9b0d4cd9d0a4
xkostik <xkostik@fi.muni.cz>
2020-11-12 22:50:20 +0100

    Now displaying info based on picked reservation

--- src/main/java/cz/muni/fi/pv168/project/gui/Forms/CheckOut.java
+++ src/main/java/cz/muni/fi/pv168/project/gui/Forms/CheckOut.java
@@ -9,11 +9,18 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.stream.Collectors;
 
 public class CheckOut extends Form implements ActionListener {
     Dimension dimension = new Dimension(300, 300);
     JButton closeButton;
-    JComboBox pickReservation;
+    JLabel label;
+    JComboBox<String> pickReservation;
+    ArrayList<Reservation> activeReservations = new ArrayList<>(Main.reservations.stream()
+            .filter((x) -> x.getStatus() == ReservationStatus.planned)
+            .collect(Collectors.toList()));
     String receipt = "<html>Client: %s<br/><br/>" +
             "Nights spent: %d<br/>" +
             "Room cost per night: %d<br/>" +
@@ -26,11 +33,12 @@ public class CheckOut extends Form implements ActionListener {
         this.setResizable(false);
         this.setLayout(new BorderLayout(0, 0));
 
-        pickReservation = new JComboBox<>(Main.reservations.stream()
-                .filter((x) -> x.getStatus() == ReservationStatus.ongoing)
-                .map(Reservation::getName).toArray());
+        pickReservation = new JComboBox<String>(activeReservations.stream()
+                .map(Reservation::getName)
+                .toArray(String[]::new));
+        pickReservation.addActionListener(this);
 
-        JLabel label = new JLabel(String.format(receipt, "Customer", 3, 1200, 50, 5200), SwingConstants.CENTER);
+        label = new JLabel("", SwingConstants.CENTER);
 
         closeButton = new Button("Close");
         closeButton.setMaximumSize(new Dimension(100, 30));
@@ -46,10 +54,18 @@ public class CheckOut extends Form implements ActionListener {
         this.add(topPanel, BorderLayout.NORTH);
     }
 
+    private void displayDetails(String name) {
+        Reservation reservation = (Reservation) activeReservations.stream()
+                .filter((x) -> x.getName().equals(name)).toArray()[0];
+        label.setText(String.format(receipt, reservation.getName(), reservation.getLength(), 1200, 50, 5200));
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == closeButton) {
             onClose();
+        } else if (e.getSource().equals(pickReservation)) {
+            displayDetails(pickReservation.getSelectedItem().toString());
         }
     }
 }
