e4c44f6e22c3882c5ce0f3a7d768c55028215a81
xkostik <xkostik@fi.muni.cz>
2020-11-12 23:37:32 +0100

    Refactored, added Cancel button

--- src/main/java/cz/muni/fi/pv168/project/gui/Forms/CheckOut.java
+++ src/main/java/cz/muni/fi/pv168/project/gui/Forms/CheckOut.java
@@ -2,6 +2,7 @@ package cz.muni.fi.pv168.project.gui.Forms;
 
 import cz.muni.fi.pv168.project.Main;
 import cz.muni.fi.pv168.project.gui.Button;
+import cz.muni.fi.pv168.project.gui.MainPanel;
 import cz.muni.fi.pv168.project.reservations.Reservation;
 import cz.muni.fi.pv168.project.reservations.ReservationStatus;
 
@@ -9,17 +10,19 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.stream.Collectors;
 
 public class CheckOut extends Form implements ActionListener {
     Dimension dimension = new Dimension(300, 300);
-    JButton closeButton;
-    JLabel label;
+    JButton outButton, cancelButton;
+    JLabel label = new JLabel("", SwingConstants.CENTER);
     JComboBox<String> pickReservation;
     ArrayList<Reservation> activeReservations = new ArrayList<>(Main.reservations.stream()
             .filter((x) -> x.getStatus() == ReservationStatus.planned)
             .collect(Collectors.toList()));
+    Reservation reservation;
     String receipt = "<html>Client: %s<br/><br/>" +
             "Nights spent: %d<br/>" +
             "Room cost per night: %d<br/>" +
@@ -32,39 +35,51 @@ public class CheckOut extends Form implements ActionListener {
         this.setResizable(false);
         this.setLayout(new BorderLayout(0, 0));
 
+        this.add(label, BorderLayout.CENTER);
+        this.add(addButtons(), BorderLayout.SOUTH);
+        this.add(addComboBox(), BorderLayout.NORTH);
+    }
+
+    private JComboBox<String> addComboBox() {
         pickReservation = new JComboBox<>(activeReservations.stream()
                 .map(Reservation::getName)
                 .toArray(String[]::new));
         pickReservation.addActionListener(this);
-
-        label = new JLabel("", SwingConstants.CENTER);
-
-        closeButton = new Button("Close");
-        closeButton.setMaximumSize(new Dimension(100, 30));
-        closeButton.addActionListener(this);
-
-        JPanel bottomPanel = new JPanel();
-        bottomPanel.add(closeButton);
-        JPanel topPanel = new JPanel();
-        topPanel.add(pickReservation, BorderLayout.CENTER);
-
-        this.add(label, BorderLayout.CENTER);
-        this.add(bottomPanel, BorderLayout.SOUTH);
-        this.add(topPanel, BorderLayout.NORTH);
+        return pickReservation;
     }
 
     private void displayDetails(String name) {
-        Reservation reservation = (Reservation) activeReservations.stream()
-                .filter((x) -> x.getName().equals(name)).toArray()[0];
+        try {
+            reservation = (Reservation) activeReservations.stream()
+                    .filter((x) -> x.getName().equals(name)).toArray()[0];
+        } catch (Exception e) {
+            throw new RuntimeException("It seems this reservation doesn't exist", e);
+        }
         label.setText(String.format(receipt, reservation.getName(), reservation.getLength(), 1200, 50, 5200));
+    }
+
+    private JPanel addButtons() {
+        outButton = new Button("Check-out");
+        outButton.addActionListener(this);
+        cancelButton = new Button("Cancel");
+        cancelButton.addActionListener(this);
+
+        JPanel panel = new JPanel();
+        panel.add(outButton);
+        panel.add(cancelButton);
+        return panel;
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == closeButton) {
+        if (e.getSource().equals(outButton)) {
+            reservation.setStatus(ReservationStatus.past);
+            MainPanel.timetable.drawWeek(LocalDate.now());
             onClose();
         } else if (e.getSource().equals(pickReservation)) {
-            displayDetails(pickReservation.getSelectedItem().toString());
+            displayDetails((String) pickReservation.getSelectedItem());
+        } else if (e.getSource().equals(cancelButton)) {
+            onClose();
         }
     }
 }
