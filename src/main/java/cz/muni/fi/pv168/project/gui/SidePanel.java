449e70fc57b203b722c07ff518eb5777f13e06ed
xkostik <xkostik@fi.muni.cz>
2020-11-13 01:08:52 +0100

    Added switching timetable displayed week based on Calendar

--- src/main/java/cz/muni/fi/pv168/project/gui/SidePanel.java
+++ src/main/java/cz/muni/fi/pv168/project/gui/SidePanel.java
@@ -1,13 +1,17 @@
 package cz.muni.fi.pv168.project.gui;
 
 import com.github.lgooddatepicker.components.CalendarPanel;
+import com.github.lgooddatepicker.optionalusertools.CalendarListener;
+import com.github.lgooddatepicker.zinternaltools.CalendarSelectionEvent;
+import com.github.lgooddatepicker.zinternaltools.YearMonthChangeEvent;
 import cz.muni.fi.pv168.project.Main;
 
 import javax.swing.*;
 import java.awt.*;
 import java.time.DayOfWeek;
+import java.time.LocalDate;
 
-public class SidePanel extends JPanel {
+public class SidePanel extends JPanel implements CalendarListener {
     public SidePanel() {
         super();
         this.setBackground(Main.backgroundColor);
@@ -22,6 +26,20 @@ public class SidePanel extends JPanel {
         CalendarPanel calendar = new CalendarPanel();
         calendar.getSettings().setFirstDayOfWeek(DayOfWeek.MONDAY);
         calendar.setBackground(new Color(240, 240, 240));
+        calendar.addCalendarListener(this);
         return calendar;
+    }
+
+    @Override
+    public void selectedDateChanged(CalendarSelectionEvent calendarSelectionEvent) {
+        if (calendarSelectionEvent.getNewDate() != null) {
+            MainPanel.timetable.drawWeek(calendarSelectionEvent.getNewDate());
+        } else {
+            MainPanel.timetable.drawWeek(LocalDate.now());
+        }
+    }
+
+    @Override
+    public void yearMonthChanged(YearMonthChangeEvent yearMonthChangeEvent) {
     }
 }
