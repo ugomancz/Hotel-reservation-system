cf9f8b7b25594b51f4e86173953150b3df45d9d9
xkostik <xkostik@fi.muni.cz>
2020-11-13 09:43:38 +0100

    Fixed duplicate getters in Reservation

--- src/main/java/cz/muni/fi/pv168/project/reservations/Reservation.java
+++ src/main/java/cz/muni/fi/pv168/project/reservations/Reservation.java
@@ -95,16 +95,8 @@ public class Reservation {
         this.length = length;
     }
 
-    public String getPhone() {
-        return phone;
-    }
-
     public void setPhone(String phone) {
         this.phone = phone;
-    }
-
-    public String getEmail() {
-        return email;
     }
 
     public void setEmail(String email) {
