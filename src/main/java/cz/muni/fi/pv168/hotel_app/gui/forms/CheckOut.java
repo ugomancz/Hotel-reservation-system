f76b8680560c7c28d70dce7d192c61260af4c800
xkostik <xkostik@fi.muni.cz>
2020-11-24 21:00:03 +0100

    Checkout now extends JDialog, "this." removed

--- src/main/java/cz/muni/fi/pv168/hotel_app/gui/forms/CheckOut.java
+++ src/main/java/cz/muni/fi/pv168/hotel_app/gui/forms/CheckOut.java
@@ -9,41 +9,42 @@ import cz.muni.fi.pv168.hotel_app.reservations.ReservationStatus;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.stream.Collectors;
 
-public class CheckOut extends Form implements ActionListener {
+public class CheckOut extends JDialog {
     private JButton outButton, cancelButton;
-    private JLabel label = new JLabel("", SwingConstants.CENTER);
+    private final JLabel label = new JLabel("", SwingConstants.CENTER);
     private JComboBox<String> pickReservation;
-    private ArrayList<Reservation> activeReservations = new ArrayList<>(Main.reservations.stream()
+    private final ArrayList<Reservation> activeReservations = new ArrayList<>(Main.reservations.stream()
             .filter((x) -> x.getStatus() == ReservationStatus.ONGOING)
             .collect(Collectors.toList()));
     private Reservation reservation;
-    private String receipt = "<html>Client: %s<br/><br/>" +
+    private final String receipt = "<html>Client: %s<br/><br/>" +
             "Nights spent: %d<br/>" +
             "Room cost per night: %d<br/>" +
             "Local fees per person per night: %d<br/><br/>" +
             "<u>Total: %d</u></html>";
 
     public CheckOut() {
-        super("Check-out");
-        this.setSize(new Dimension(250, 250));
-        this.setResizable(false);
-        this.setLayout(new BorderLayout(0, 0));
+        super(Main.frame, "Check-out", ModalityType.APPLICATION_MODAL);
+        setSize(new Dimension(250, 250));
+        setResizable(false);
+        setLayout(new BorderLayout(0, 0));
+        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
 
-        this.add(label, BorderLayout.CENTER);
-        this.add(addButtons(), BorderLayout.SOUTH);
-        this.add(addComboBox(), BorderLayout.NORTH);
+        add(label, BorderLayout.CENTER);
+        add(addButtons(), BorderLayout.SOUTH);
+        add(addComboBox(), BorderLayout.NORTH);
+        setVisible(true);
     }
 
     private JPanel addButtons() {
         outButton = new Button("Check-out");
-        outButton.addActionListener(this);
+        outButton.addActionListener(this::actionPerformed);
         cancelButton = new Button("Cancel");
-        cancelButton.addActionListener(this);
+        cancelButton.addActionListener(this::actionPerformed);
 
         JPanel panel = new JPanel();
         panel.add(outButton);
@@ -55,7 +56,7 @@ public class CheckOut extends Form implements ActionListener {
         pickReservation = new JComboBox<>(activeReservations.stream()
                 .map(Reservation::getName)
                 .toArray(String[]::new));
-        pickReservation.addActionListener(this);
+        pickReservation.addActionListener(this::actionPerformed);
         return pickReservation;
     }
 
@@ -69,16 +70,15 @@ public class CheckOut extends Form implements ActionListener {
         label.setText(String.format(receipt, reservation.getName(), reservation.getLength(), 1200, 50, 5200));
     }
 
-    @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource().equals(outButton)) {
             reservation.setStatus(ReservationStatus.PAST);
             MainPanel.timetable.drawWeek(LocalDate.now());
-            onClose();
+            dispose();
         } else if (e.getSource().equals(pickReservation)) {
             displayDetails((String) pickReservation.getSelectedItem());
         } else if (e.getSource().equals(cancelButton)) {
-            onClose();
+            dispose();
         }
     }
 }
