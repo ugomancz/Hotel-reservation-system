2fd0c561fc25f1fd6d508173b95e719c8168a868
Denis Kollar <493161@mail.muni.cz>
2020-11-25 20:23:12 +0100

    Added createTable method

--- src/main/java/cz/muni/fi/pv168/hotel_app/data/ReservationDao.java
+++ src/main/java/cz/muni/fi/pv168/hotel_app/data/ReservationDao.java
@@ -2,6 +2,7 @@ package cz.muni.fi.pv168.hotel_app.data;
 
 
 import javax.sql.DataSource;
+import java.sql.SQLException;
 
 
 /**
@@ -12,6 +13,22 @@ public final class ReservationDao {
 
     public ReservationDao(DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    private void createTable() {
+        try (var connection = dataSource.getConnection();
+             var st = connection.createStatement()) {
+
+            st.executeUpdate("CREATE TABLE APP.EMPLOYEE (" +
+                    "ID BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY," +
+                    "FIRST_NAME VARCHAR(100) NOT NULL," +
+                    "LAST_NAME VARCHAR(100) NOT NULL," +
+                    "GENDER VARCHAR(6) NOT NULL CONSTRAINT GENDER_CHECK CHECK (GENDER IN ('MALE','FEMALE'))," +
+                    "BIRTH_DATE DATE NOT NULL" +
+                    ")");
+        } catch (SQLException ex) {
+            throw new DataAccessException("Failed to create EMPLOYEE table", ex);
+        }
     }
 
 }
