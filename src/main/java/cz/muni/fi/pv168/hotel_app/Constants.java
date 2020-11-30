b2ac28fa54c38296101daf542b304fd26d4bbed4
xkostik <xkostik@fi.muni.cz>
2020-11-30 13:04:54 +0100

    ReservationStatus colors moved to Constants

--- src/main/java/cz/muni/fi/pv168/hotel_app/Constants.java
+++ src/main/java/cz/muni/fi/pv168/hotel_app/Constants.java
@@ -7,6 +7,9 @@ public final class Constants {
     public final static Color BACKGROUND_COLOR = Color.lightGray;
     public final static int NUMBER_OF_ROOMS = 15;
     public final static int DAYS_IN_WEEK = 7;
+    public final static Color PLANNED_RESERVATION = new Color(13,218,13);
+    public final static Color ONGOING_RESERVATION = Color.orange;
+    public final static Color PAST_RESERVATION = Color.lightGray;
 
     public Constants() {
         throw new AssertionError();
--- src/main/java/cz/muni/fi/pv168/hotel_app/reservations/ReservationStatus.java
+++ src/main/java/cz/muni/fi/pv168/hotel_app/reservations/ReservationStatus.java
@@ -1,24 +1,26 @@
 package cz.muni.fi.pv168.hotel_app.reservations;
 
+import cz.muni.fi.pv168.hotel_app.Constants;
+
 import java.awt.*;
 
 public enum ReservationStatus {
     PLANNED {
         @Override
         public Color getColor() {
-            return Color.green;
+            return Constants.PLANNED_RESERVATION;
         }
     },
     ONGOING {
         @Override
         public Color getColor() {
-            return Color.orange;
+            return Constants.ONGOING_RESERVATION;
         }
     },
     PAST {
         @Override
         public Color getColor() {
-            return Color.lightGray;
+            return Constants.PAST_RESERVATION;
         }
     };
 
