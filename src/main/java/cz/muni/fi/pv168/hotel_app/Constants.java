aedbe85a29079400940f3475e455ef6976fca0b6
Lukas Hasik <xhasik@fi.muni.cz>
2020-12-01 18:59:29 +0100

    Unused imports from Constants deleted

--- src/main/java/cz/muni/fi/pv168/hotel_app/Constants.java
+++ src/main/java/cz/muni/fi/pv168/hotel_app/Constants.java
@@ -1,8 +1,5 @@
 package cz.muni.fi.pv168.hotel_app;
 
-import cz.muni.fi.pv168.hotel_app.rooms.BedType;
-import cz.muni.fi.pv168.hotel_app.rooms.Room;
-import cz.muni.fi.pv168.hotel_app.rooms.RoomType;
 
 import java.awt.*;
 
@@ -11,11 +8,10 @@ public final class Constants {
     public final static Color BACKGROUND_COLOR = Color.lightGray;
     public final static int NUMBER_OF_ROOMS = 15;
     public final static int DAYS_IN_WEEK = 7;
-    public final static Color PLANNED_RESERVATION = new Color(13,218,13);
+    public final static Color PLANNED_RESERVATION = new Color(13, 218, 13);
     public final static Color ONGOING_RESERVATION = Color.orange;
     public final static Color PAST_RESERVATION = Color.lightGray;
-    public final static Color CONCURRING_RESERVATIONS = new Color(60,160,50);
-
+    public final static Color CONCURRING_RESERVATIONS = new Color(60, 160, 50);
 
 
     public Constants() {
