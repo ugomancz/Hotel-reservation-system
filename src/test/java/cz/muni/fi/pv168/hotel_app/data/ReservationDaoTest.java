8c62082b8418f06f0d9fba65c6728a98bca2e806
Lukas Hasik <xhasik@fi.muni.cz>
2020-12-03 16:18:42 +0100

    Test findAll added

--- src/test/java/cz/muni/fi/pv168/hotel_app/data/ReservationDaoTest.java
+++ src/test/java/cz/muni/fi/pv168/hotel_app/data/ReservationDaoTest.java
@@ -59,6 +59,34 @@ public class ReservationDaoTest {
                 .isEmpty();
     }
 
+    @Test
+    void findAll() {
+        var testRes1 = new Reservation("Tester Smith", "777777777", "tester@test.com", 4, 2,
+                LocalDate.now(), LocalDate.now().plusDays(4), "PLANNED");
+        var testRes2 = new Reservation("Testerina Hero", "77769777", "testerina@hero.com", 2, 1,
+                LocalDate.now(), LocalDate.now().plusDays(4), "PLANNED");
+        var testRes3 = new Reservation("Tester Jester", "778654777", "tester@test.com", 1, 3,
+                LocalDate.now(), LocalDate.now().plusDays(4), "PLANNED");
+        var testRes4 = new Reservation("Tester J. Morgan", "771237777", "tmorg@test.com", 4, 5,
+                LocalDate.now(), LocalDate.now().plusDays(4), "PLANNED");
+        var testRes5 = new Reservation("Test R. Boy", "777333477", "tester@test.com", 5, 8,
+                LocalDate.now(), LocalDate.now().plusDays(4), "PLANNED");
+        var testRes6 = new Reservation("T. S. Ter", "420777769", "tester@test.com", 4, 10,
+                LocalDate.now(), LocalDate.now().plusDays(4), "PLANNED");
+
+
+        reservationDao.create(testRes1);
+        reservationDao.create(testRes2);
+        reservationDao.create(testRes3);
+        reservationDao.create(testRes4);
+        reservationDao.create(testRes5);
+        reservationDao.create(testRes6);
+
+        assertThat(reservationDao.findAll())
+                .usingFieldByFieldElementComparator()
+                .containsExactlyInAnyOrder(testRes1, testRes2, testRes3, testRes4, testRes5, testRes6);
+    }
+
 
 
 
