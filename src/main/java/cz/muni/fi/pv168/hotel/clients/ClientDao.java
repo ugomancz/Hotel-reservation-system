452383fbb3a0a4199cf3f7f3c1a6b203383699af
Denis Kollar <493161@mail.muni.cz>
2020-12-22 03:01:08 +0100

    Added crud without read into ClientDao

--- src/main/java/cz/muni/fi/pv168/hotel/clients/ClientDao.java
+++ src/main/java/cz/muni/fi/pv168/hotel/clients/ClientDao.java
@@ -1,9 +1,13 @@
 package cz.muni.fi.pv168.hotel.clients;
 
 import cz.muni.fi.pv168.hotel.DataAccessException;
+import cz.muni.fi.pv168.hotel.reservations.Reservation;
 
 import javax.sql.DataSource;
+import java.sql.Date;
 import java.sql.SQLException;
+
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
 
 /**
  * @author Denis Kollar
@@ -15,6 +19,63 @@ public class ClientDao {
         this.dataSource = dataSource;
         if (!tableExits("APP", "CLIENT")) {
             createTable();
+        }
+    }
+
+    public void create(Client client) {
+        try (var connection = dataSource.getConnection();
+             var st = connection.prepareStatement(
+                     "INSERT INTO CLIENT (FNAME, LNAME, GUESTID) VALUES (?, ?, ?)",
+                     RETURN_GENERATED_KEYS)) {
+            st.setString(1, client.getFirstName());
+            st.setString(2, client.getLastName());
+            st.setString(3, client.getGuestId());
+            st.executeUpdate();
+            try (var rs = st.getGeneratedKeys()) {
+                if (rs.next()) {
+                    client.setId(rs.getLong(1));
+                } else {
+                    throw new DataAccessException("Failed to fetch generated key: no key returned for client: " + client);
+                }
+            }
+        } catch (SQLException ex) {
+            throw new DataAccessException("Failed to store client " + client, ex);
+        }
+    }
+
+    public void delete(Reservation client) {
+        if (client.getId() == null) {
+            throw new IllegalArgumentException("Client has null ID");
+        }
+        try (var connection = dataSource.getConnection();
+             var st = connection.prepareStatement("DELETE FROM CLIENT WHERE ID = ?")) {
+            st.setLong(1, client.getId());
+            int rowsDeleted = st.executeUpdate();
+            if (rowsDeleted == 0) {
+                throw new DataAccessException("Failed to delete non-existing client: " + client);
+            }
+        } catch (SQLException ex) {
+            throw new DataAccessException("Failed to delete client " + client, ex);
+        }
+    }
+
+    public void update(Client client) {
+        if (client.getId() == null) {
+            throw new IllegalArgumentException("Client has null ID");
+        }
+        try (var connection = dataSource.getConnection();
+             var st = connection.prepareStatement(
+                     "UPDATE CLIENT SET FNAME = ?, LNAME = ?, GUESTID = ? WHERE ID = ?")) {
+            st.setString(1, client.getFirstName());
+            st.setString(2, client.getLastName());
+            st.setString(3, client.getGuestId());
+            st.setLong(4, client.getId());
+            int rowsUpdated = st.executeUpdate();
+            if (rowsUpdated == 0) {
+                throw new DataAccessException("Failed to update non-existing client: " + client);
+            }
+        } catch (SQLException ex) {
+            throw new DataAccessException("Failed to update client " + client, ex);
         }
     }
 
