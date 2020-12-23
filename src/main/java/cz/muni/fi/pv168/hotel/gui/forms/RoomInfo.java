3336ef426a7efda15d892626da7f1a342452b6aa
Timotej Cirok <xcirok@fi.muni.cz>
2020-12-23 11:09:39 +0100

    RoomInfo dependency fixed

--- src/main/java/cz/muni/fi/pv168/hotel/gui/forms/RoomInfo.java
+++ src/main/java/cz/muni/fi/pv168/hotel/gui/forms/RoomInfo.java
@@ -12,25 +12,25 @@ import java.awt.Color;
 import java.awt.Dialog;
 import java.awt.Dimension;
 
-public class RoomInfo extends JDialog {
+public class RoomInfo{
 
     private static final I18N I18N = new I18N(RoomInfo.class);
 
     public RoomInfo(JFrame frame) {
-        super(frame, I18N.getString("windowTitle"), Dialog.ModalityType.APPLICATION_MODAL);
-        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-        setLocationRelativeTo(frame);
-        setMinimumSize(new Dimension(400, 360));
+        JDialog dialog = new JDialog(frame, I18N.getString("windowTitle"), Dialog.ModalityType.APPLICATION_MODAL);
+        dialog.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        dialog.setLocationRelativeTo(frame);
+        dialog.setMinimumSize(new Dimension(400, 360));
         JTextArea area = new JTextArea();
         area.setEditable(false);
         area.setBorder(new EmptyBorder(5, 5, 5, 5));
-        setBackground(new Color(240, 240, 240));
+        dialog.setBackground(new Color(240, 240, 240));
 
         for (int i = 0; i < 15; i++) {
             area.append(RoomDao.getRoom(i + 1).toString() + "\n");
         }
-        add(area);
-        pack();
-        setVisible(true);
+        dialog.add(area);
+        dialog.pack();
+        dialog.setVisible(true);
     }
 }
