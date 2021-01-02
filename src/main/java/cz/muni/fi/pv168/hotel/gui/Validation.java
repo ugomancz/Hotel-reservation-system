4c3dd1b35bc15134dfe4fdede421926033538c99
xkostik <xkostik@fi.muni.cz>
2021-01-02 03:14:05 +0100

    Validation class updated

--- src/main/java/cz/muni/fi/pv168/hotel/gui/Validation.java
+++ src/main/java/cz/muni/fi/pv168/hotel/gui/Validation.java
@@ -1,9 +1,15 @@
 package cz.muni.fi.pv168.hotel.gui;
 
+import org.apache.commons.validator.routines.EmailValidator;
+
 /**
  * @author Ondrej Kostik
  */
 public class Validation {
+
+    private Validation() {
+        throw new AssertionError();
+    }
 
     public static boolean isNumeric(String input) {
         try {
@@ -14,7 +20,11 @@ public class Validation {
         }
     }
 
-    private Validation() {
-        throw new AssertionError();
+    public static boolean isEmail(String input) {
+        return EmailValidator.getInstance().isValid(input);
+    }
+
+    public static boolean isAlpha(String input) {
+        return input.chars().allMatch(Character::isLetter);
     }
 }
