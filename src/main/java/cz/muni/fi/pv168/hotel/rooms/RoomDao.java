5c4f4a9a52f7a7386c702ae1933becde46d62732
Denis Kollar <493161@mail.muni.cz>
2021-01-06 23:07:52 +0100

    Added findAll() into RoomDao.java // Fixed room numbers

--- src/main/java/cz/muni/fi/pv168/hotel/rooms/RoomDao.java
+++ src/main/java/cz/muni/fi/pv168/hotel/rooms/RoomDao.java
@@ -21,18 +21,18 @@ public final class RoomDao {
             Map.entry(103, new Room(103, 1500, 1, 0)),
             Map.entry(104, new Room(104, 1500, 1, 0)),
             // first floor, two separate beds
-            Map.entry(105, new Room(105, 1500, 2, 0)),
+            Map.entry(105, new Room(105, 2800, 2, 0)),
             Map.entry(106, new Room(106, 2800, 2, 0)),
             // second floor, king-size bed
             Map.entry(201, new Room(201, 2900, 0, 1)),
             Map.entry(202, new Room(202, 2800, 0, 1)),
             Map.entry(203, new Room(203, 2800, 0, 1)),
             // second floor, king-size + one single bed
-            Map.entry(204, new Room(10, 2900, 1, 1)),
-            Map.entry(205, new Room(11, 2800, 1, 1)),
+            Map.entry(204, new Room(204, 2900, 1, 1)),
+            Map.entry(205, new Room(205, 2800, 1, 1)),
             // third floor, apartments (king-size + two single beds)
-            Map.entry(301, new Room(301, 2850, 2, 1)),
-            Map.entry(302, new Room(302, 2800, 2, 1)),
+            Map.entry(301, new Room(301, 4200, 2, 1)),
+            Map.entry(302, new Room(302, 4200, 2, 1)),
             Map.entry(303, new Room(303, 4200, 2, 1))
     );
 
