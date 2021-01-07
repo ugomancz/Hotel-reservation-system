198dd31fece5659ab1c486ecb0f409b19ab91a7b
xkostik <xkostik@fi.muni.cz>
2021-01-07 19:00:54 +0100

    Room prices added to Constants

--- src/main/java/cz/muni/fi/pv168/hotel/Constants.java
+++ src/main/java/cz/muni/fi/pv168/hotel/Constants.java
@@ -1,7 +1,10 @@
 package cz.muni.fi.pv168.hotel;
 
+import cz.muni.fi.pv168.hotel.rooms.RoomDao;
+
 import java.awt.Color;
 import java.awt.Font;
+import java.util.Map;
 
 public final class Constants {
 
@@ -11,7 +14,12 @@ public final class Constants {
     public static final String DELIMITER = ";";
     public static final Font BUTTON_FONT = new Font("Helvetica", Font.BOLD, 14);
     public static final Color BUTTON_BACKGROUND = new Color(240, 240, 240);
-
+    public static final Map<RoomDao.RoomPriceCategory, Integer> ROOM_PRICES = Map.ofEntries(
+            Map.entry(RoomDao.RoomPriceCategory.SINGLE_ROOM, 849),
+            Map.entry(RoomDao.RoomPriceCategory.DOUBLE_ROOM, 1199),
+            Map.entry(RoomDao.RoomPriceCategory.TRIPLE_ROOM, 1649),
+            Map.entry(RoomDao.RoomPriceCategory.APARTMENT, 2199)
+    );
     private Constants() {
         throw new AssertionError();
     }
