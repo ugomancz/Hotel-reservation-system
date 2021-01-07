2c137e7a607b043f33c273e4ea15c9cebddd477b
Denis Kollar <493161@mail.muni.cz>
2021-01-07 23:39:03 +0100

    Changed Room and RoomDao to accept RoomPriceCategory, not Price

--- src/main/java/cz/muni/fi/pv168/hotel/rooms/RoomDao.java
+++ src/main/java/cz/muni/fi/pv168/hotel/rooms/RoomDao.java
@@ -18,24 +18,24 @@ public final class RoomDao {
 
     private final Map<Integer, Room> rooms = Map.ofEntries(
             // first floor, one bed
-            Map.entry(101, new Room(101, Constants.ROOM_PRICES.get(RoomPriceCategory.SINGLE_ROOM), 1, 0)),
-            Map.entry(102, new Room(102, Constants.ROOM_PRICES.get(RoomPriceCategory.SINGLE_ROOM), 1, 0)),
-            Map.entry(103, new Room(103, Constants.ROOM_PRICES.get(RoomPriceCategory.SINGLE_ROOM), 1, 0)),
-            Map.entry(104, new Room(104, Constants.ROOM_PRICES.get(RoomPriceCategory.SINGLE_ROOM), 1, 0)),
+            Map.entry(101, new Room(101, RoomPriceCategory.SINGLE_ROOM, 1, 0)),
+            Map.entry(102, new Room(102, RoomPriceCategory.SINGLE_ROOM, 1, 0)),
+            Map.entry(103, new Room(103, RoomPriceCategory.SINGLE_ROOM, 1, 0)),
+            Map.entry(104, new Room(104, RoomPriceCategory.SINGLE_ROOM, 1, 0)),
             // first floor, two separate beds
-            Map.entry(105, new Room(105, Constants.ROOM_PRICES.get(RoomPriceCategory.DOUBLE_ROOM), 2, 0)),
-            Map.entry(106, new Room(106, Constants.ROOM_PRICES.get(RoomPriceCategory.DOUBLE_ROOM), 2, 0)),
+            Map.entry(105, new Room(105, RoomPriceCategory.DOUBLE_ROOM, 2, 0)),
+            Map.entry(106, new Room(106, RoomPriceCategory.DOUBLE_ROOM, 2, 0)),
             // second floor, king-size bed
-            Map.entry(201, new Room(201, Constants.ROOM_PRICES.get(RoomPriceCategory.DOUBLE_ROOM), 0, 1)),
-            Map.entry(202, new Room(202, Constants.ROOM_PRICES.get(RoomPriceCategory.DOUBLE_ROOM), 0, 1)),
-            Map.entry(203, new Room(203, Constants.ROOM_PRICES.get(RoomPriceCategory.DOUBLE_ROOM), 0, 1)),
+            Map.entry(201, new Room(201, RoomPriceCategory.DOUBLE_ROOM, 0, 1)),
+            Map.entry(202, new Room(202, RoomPriceCategory.DOUBLE_ROOM, 0, 1)),
+            Map.entry(203, new Room(203, RoomPriceCategory.DOUBLE_ROOM, 0, 1)),
             // second floor, king-size + one single bed
-            Map.entry(204, new Room(204, Constants.ROOM_PRICES.get(RoomPriceCategory.TRIPLE_ROOM), 1, 1)),
-            Map.entry(205, new Room(205, Constants.ROOM_PRICES.get(RoomPriceCategory.TRIPLE_ROOM), 1, 1)),
+            Map.entry(204, new Room(204, RoomPriceCategory.TRIPLE_ROOM, 1, 1)),
+            Map.entry(205, new Room(205, RoomPriceCategory.TRIPLE_ROOM, 1, 1)),
             // third floor, apartments (king-size + two single beds)
-            Map.entry(301, new Room(301, Constants.ROOM_PRICES.get(RoomPriceCategory.APARTMENT), 2, 1)),
-            Map.entry(302, new Room(302, Constants.ROOM_PRICES.get(RoomPriceCategory.APARTMENT), 2, 1)),
-            Map.entry(303, new Room(303, Constants.ROOM_PRICES.get(RoomPriceCategory.APARTMENT), 2, 1))
+            Map.entry(301, new Room(301, RoomPriceCategory.APARTMENT, 2, 1)),
+            Map.entry(302, new Room(302, RoomPriceCategory.APARTMENT, 2, 1)),
+            Map.entry(303, new Room(303, RoomPriceCategory.APARTMENT, 2, 1))
     );
     public RoomDao(DataSource dataSource) {
         this.dataSource = dataSource;
@@ -47,7 +47,7 @@ public final class RoomDao {
 
     public Room getRoom(int roomNumber) {
         try (var connection = dataSource.getConnection();
-             var st = connection.prepareStatement("SELECT ROOMNUMBER, PRICE, STANDARD, KINGSIZE " +
+             var st = connection.prepareStatement("SELECT ROOMNUMBER, PRICECATEGORY, STANDARD, KINGSIZE " +
                      "FROM ROOM WHERE ROOMNUMBER=?")) {
             st.setInt(1, roomNumber);
             return createRoom(st);
@@ -94,22 +94,6 @@ public final class RoomDao {
         }
     }
 
-    public void printAll() {
-        try (var connection = dataSource.getConnection();
-             var st = connection.prepareStatement("SELECT ROOMNUMBER, PRICE, STANDARD, KINGSIZE FROM ROOM")) {
-            try (var rs = st.executeQuery()) {
-                while (rs.next()) {
-                    System.out.println(rs.getInt("ROOMNUMBER") + " " +
-                            rs.getInt("PRICE") + " " +
-                            rs.getInt("STANDARD") + " " +
-                            rs.getInt("KINGSIZE"));
-                }
-            }
-        } catch (SQLException ex) {
-            throw new DataAccessException("Failed to load all reservations", ex);
-        }
-    }
-
     public void printAll(List<Room> rooms) {
         for (Room room :
                 rooms) {
@@ -118,20 +102,9 @@ public final class RoomDao {
     }
 
     public List<Room> findAll() {
-        ArrayList<Room> rooms = new ArrayList<>();
         try (var connection = dataSource.getConnection();
-             var st = connection.prepareStatement("SELECT ROOMNUMBER, PRICE, STANDARD, KINGSIZE FROM ROOM ORDER BY ROOMNUMBER")) {
-            try (var rs = st.executeQuery()) {
-                while (rs.next()) {
-                    rooms.add(new Room(
-                            rs.getInt("ROOMNUMBER"),
-                            rs.getInt("PRICE"),
-                            rs.getInt("STANDARD"),
-                            rs.getInt("KINGSIZE")
-                    ));
-                }
-                return rooms;
-            }
+             var st = connection.prepareStatement("SELECT ROOMNUMBER, PRICECATEGORY, STANDARD, KINGSIZE FROM ROOM ORDER BY ROOMNUMBER")) {
+            return createRooms(st);
         } catch (SQLException ex) {
             throw new DataAccessException("Failed to load all reservations", ex);
         }
@@ -140,7 +113,7 @@ public final class RoomDao {
     private void create(Room room) {
         try (var connection = dataSource.getConnection();
              var st = connection.prepareStatement(
-                     "INSERT INTO ROOM (ROOMNUMBER, PRICE, STANDARD, KINGSIZE) VALUES (?, ?, ?, ?)",
+                     "INSERT INTO ROOM (ROOMNUMBER, PRICECATEGORY, STANDARD, KINGSIZE) VALUES (?, ?, ?, ?)",
                      RETURN_GENERATED_KEYS)) {
             setRows(room, st);
             if (st.executeUpdate() == 0) {
@@ -153,7 +126,7 @@ public final class RoomDao {
 
     private void setRows(Room room, PreparedStatement st) throws SQLException {
         st.setInt(1, room.getRoomNumber());
-        st.setInt(2, room.getPricePerNight());
+        st.setString(2, room.getRoomPriceCategory().name());
         st.setInt(3, room.getStandardBeds());
         st.setInt(4, room.getKingsizeBeds());
     }
@@ -162,8 +135,11 @@ public final class RoomDao {
         List<Room> rooms = new ArrayList<>();
         try (var rs = st.executeQuery()) {
             while (rs.next()) {
-                Room room = new Room(rs.getInt("ROOMNUMBER"),rs.getInt("PRICE"),
-                        rs.getInt("KINGSIZE"),rs.getInt("STANDARD"));
+                Room room = new Room(
+                        rs.getInt("ROOMNUMBER"),
+                        RoomPriceCategory.valueOf(rs.getString("PRICECATEGORY")),
+                        rs.getInt("KINGSIZE"),
+                        rs.getInt("STANDARD"));
                 rooms.add(room);
             }
         }
@@ -174,7 +150,11 @@ public final class RoomDao {
 
         try (var rs = st.executeQuery()) {
             if (rs.next()) {
-                return new Room(rs.getInt("ROOMNUMBER"), rs.getInt("PRICE"), rs.getInt("STANDARD"), rs.getInt("KINGSIZE"));
+                return new Room(
+                        rs.getInt("ROOMNUMBER"),
+                        RoomPriceCategory.valueOf(rs.getString("PRICECATEGORY")),
+                        rs.getInt("STANDARD"),
+                        rs.getInt("KINGSIZE"));
             } else {
                 throw new SQLException();
             }
@@ -196,7 +176,7 @@ public final class RoomDao {
 
             st.executeUpdate("CREATE TABLE APP.ROOM (" +
                     "ROOMNUMBER INT PRIMARY KEY," +
-                    "PRICE INT," +
+                    "PRICECATEGORY VARCHAR(255)," +
                     "STANDARD INT," +
                     "KINGSIZE INT)");
         } catch (SQLException ex) {
