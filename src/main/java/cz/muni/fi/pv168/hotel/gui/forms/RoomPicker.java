27c3e0640ebad809267133a1f02efa004b7869cf
xkostik <xkostik@fi.muni.cz>
2021-01-07 11:39:28 +0100

    New RoomDao implemented

--- src/main/java/cz/muni/fi/pv168/hotel/gui/forms/RoomPicker.java
+++ src/main/java/cz/muni/fi/pv168/hotel/gui/forms/RoomPicker.java
@@ -1,26 +1,29 @@
 package cz.muni.fi.pv168.hotel.gui.forms;
 
 import cz.muni.fi.pv168.hotel.gui.I18N;
+import cz.muni.fi.pv168.hotel.rooms.Room;
 import cz.muni.fi.pv168.hotel.rooms.RoomDao;
 
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
+import java.util.List;
 
 public class RoomPicker {
 
     private static final I18N I18N = new I18N(RoomPicker.class);
+    private static List<Room> rooms;
 
     static JTable createTable(RoomDao roomDao) {
-        DefaultTableModel dataModel = new DesignedTableModel(roomDao);
-
+        rooms = roomDao.findAll();
+        DefaultTableModel dataModel = new DesignedTableModel();
         JTable table = new JTable(dataModel);
         table.getColumnModel().getColumn(0).setMinWidth(40);
         table.getColumnModel().getColumn(0).setMaxWidth(40);
         table.getColumnModel().getColumn(0).setResizable(false);
-        for (int i = 0; i < roomDao.numberOfRooms(); i++) {
+        for (int i = 0; i < rooms.size(); i++) {
             dataModel.addRow(new Object[0]);
             dataModel.setValueAt(false, i, 0);
-            dataModel.setValueAt(roomDao.getRoom(i + 1).toString(), i, 1);
+            dataModel.setValueAt(rooms.get(i).toString(), i, 1);
         }
         return table;
     }
@@ -30,8 +33,8 @@ public class RoomPicker {
         private final String[] columns = {I18N.getString("checkColumn"), I18N.getString("descriptionColumn")};
         private final boolean[][] editable_cells;
 
-        DesignedTableModel(RoomDao roomDao) {
-            this.editable_cells = new boolean[roomDao.numberOfRooms()][columns.length];
+        DesignedTableModel() {
+            this.editable_cells = new boolean[rooms.size()][columns.length];
         }
 
         public int getColumnCount() {
