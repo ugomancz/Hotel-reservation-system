8189601043dff3ed5aa4ceacd3f84079d560e0b8
xkostik <xkostik@fi.muni.cz>
2021-01-08 21:47:38 +0100

    Test to check all rooms added

--- src/main/java/cz/muni/fi/pv168/hotel/rooms/Room.java
+++ src/main/java/cz/muni/fi/pv168/hotel/rooms/Room.java
@@ -3,6 +3,8 @@ package cz.muni.fi.pv168.hotel.rooms;
 import cz.muni.fi.pv168.hotel.Constants;
 import cz.muni.fi.pv168.hotel.gui.I18N;
 
+import java.util.Objects;
+
 public class Room {
 
     private static final I18N I18N = new I18N(Room.class);
@@ -52,6 +54,20 @@ public class Room {
 
     public void setKingsizeBeds(int kingsizeBeds) {
         this.kingsizeBeds = kingsizeBeds;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Room room = (Room) o;
+        return getRoomNumber() == room.getRoomNumber() && getStandardBeds() == room.getStandardBeds()
+                && getKingsizeBeds() == room.getKingsizeBeds() && getRoomPriceCategory() == room.getRoomPriceCategory();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getRoomNumber(), getRoomPriceCategory(), getStandardBeds(), getKingsizeBeds());
     }
 
     @Override
